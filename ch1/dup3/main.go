26b9087a150b7989053b532e61e50fead2ab6bb1
Roberto Calderon <roberto@robertocalderon.ca>
2016-03-08 23:31:34 -0800

    solved exercise dup3

--- ch1/dup3/main.go
+++ ch1/dup3/main.go
@@ -17,20 +17,23 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]map[string]int)
 	for _, filename := range os.Args[1:] {
 		data, err := ioutil.ReadFile(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
 		}
+		counts[filename] = make(map[string]int)
 		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
+			counts[filename][line]++
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for filename, lines := range counts {
+		for line, n := range lines {
+			if n > 1 {
+				fmt.Printf("%d\t%s\t%s\n", n, filename, line)
+			}
 		}
 	}
 }
