a57946f85891211e001428707b8a0f2df30d9397
Roberto Calderon <roberto@robertocalderon.ca>
2016-03-12 21:26:43 -0800

    parse cycle parameter in url

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -16,6 +16,7 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strconv"
 )
 
 //!-main
@@ -45,7 +46,11 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
+			c, err := strconv.Atoi(r.FormValue("cycles"))
+			if err != nil {
+				log.Printf("Error formatting query param %s", err)
+			}
+			lissajous(w, c)
 		}
 		http.HandleFunc("/", handler)
 		//!-http
@@ -53,12 +58,11 @@ func main() {
 		return
 	}
 	//!+main
-	lissajous(os.Stdout)
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, cycles int) {
 	const (
-		cycles  = 5     // number of complete x oscillator revolutions
+		//cycles  = lk    // number of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
@@ -70,7 +74,7 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
